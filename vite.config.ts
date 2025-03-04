fd3afb18a5ded622bddc443c5a24ee2be40ea3a4
soulrobert001 <johnrobertdestiny@gmail.com>
2025-03-04 23:45:25 +0800

    优化 Vercel 部署配置

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,14 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          antd: ['antd', '@ant-design/icons']
+        }
+      }
+    }
   }
 })
